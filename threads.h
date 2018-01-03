d54e33471a0ff5b2d371f999be773b0d351e3cc4
weik <klaudia.weigel1101@gmail.com>
2018-01-03 18:40:17 +0100

    added threads.h

---
+++ threads.h
@@ -0,0 +1,13 @@
+#ifndef THREADS_H
+#define THREADS_H
+
+void libinit();
+
+int thread_create(void *(*func)(void *), void *arg);
+
+void thread_join(int thread_tid, void **retval);
+
+void thread_exit(void *retval);
+
+#endif
+    
