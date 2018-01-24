dc6372611a8680f03ea78f17850602c62d5cca4b
weik <klaudia.weigel1101@gmail.com>
2018-01-24 18:24:22 +0100

    new header

--- threads.h
+++ threads.h
@@ -1,13 +1,18 @@
 #ifndef THREADS_H
 #define THREADS_H
 
+// creating main thread
 void libinit();
 
-int thread_create(void *(*func)(void *), void *arg);
+// creating a thread a putting it on a queue
+int thread_create(void *(*start_routine)(void *), void *arg);
 
+// for now assuming that retval is NULL, ! TODO later
 void thread_join(int thread_tid, void **retval);
 
 void thread_exit(void *retval);
+
+void thread_yield();
 
 #endif
     
